e6b3cdcf0c13cf4342c67daeebd6cff8cfd05b72
liucaijun <liucaijun@jd.com>
2020-08-27 21:26:23 +0800

    fix style

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,38 +2,31 @@
   <div class="wrap">
     <div class="todolist">
       <div class="collapse-panel" bindtap="handleCollapseEvent" data-type="todo">
-        <i class="spread" :class="{'collapse-rotate' : todo.collapse}"></i>待完成
-        <i class="todo-icon"></i>{{todoNum}}
+        <div class='center'><div class="spread" :class="{'collapse-rotate' : todo.collapse}"></div>待完成</div>
+        <div class='center'><div class="todo-icon"></div>{{todoNum}}</div>
       </div>
-      <div v-if="todo.isShowed" v-for="item in todoList" :key="item.id" class="list">
-        <div
-          v-if="!item.isFinished"
-          class="item-container list"
-          @click="EditTodo"
-        >
-          <div class="center item-left">
-            <checkbox
-              class="item__check"
-              checked="item.isFinished ? 'checked' : ''"
-              catchtap="bindToggle"
-              v-model="item.id"
-            />
-            <div class="item__main">
-              <label class="item__main__text">{{item.value}}</label>
-              <label v-if="item.endDate" class="item__main__date">{{item.endDate}}</label>
-              <label v-if="item.endTime" class="item__main__time">{{item.endTime}}</label>
+      <div v-if="todo.isShowed">
+        <div v-for="item in todoList" :key="item.id" class="list">
+          <div v-if="!item.isFinished" class="item-container list" @click="EditTodo">
+            <div class="center item-left">
+              <input type="checkbox" class="item__check" :checked="item.isFinished ? 'checked' : ''" @click="ToggleTodo" v-model="item.id"/>
+              <div class="item__main">
+                <label class="item__main__text">{{item.value}}</label>
+                <label v-if="item.endDate" class="item__main__date">{{item.endDate}}</label>
+                <label v-if="item.endTime" class="item__main__time">{{item.endTime}}</label>
+              </div>
             </div>
+            <i class="del-icon" catchtap="bindDelete" :id="item.id"></i>
           </div>
-          <i class="del-icon" catchtap="bindDelete" :id="item.id"></i>
         </div>
       </div>
-<!-- <div v-if="!todoNum && todo.isShowed" class="empty center list--todo">
+      <div v-if="!todoNum && todo.isShowed" class="empty center list--todo">
         <i class="box-icon"></i>
         <text>你还没有添加任务呢~</text>
-      </div> -->
-</div>
-<div class="donelist">
-    <!-- <div
+      </div>
+    </div>
+    <div class="donelist">
+      <!-- <div
         class="[collapse-panel,collapse-panel--done,done.isShowed ? '' : 'list--done']"
         bindtap="handleCollapseEvent"
         data-type="done"
@@ -69,32 +62,41 @@
         <i class="box-icon"></i>
         <text>空空如也~</text>
       </div>-->
-</div>
-</div>
+    </div>
+  </div>
 </template>
 <script>
-    export default {
-        data: function() {
-            return {
-                isActive: true,
-                todo: {
-                    collapse: false,
-                    isShowed: true
-                },
-                todoNum: 12,
-                todoList: [{
-                    id: 1,
-                    value: 'hhhhha',
-                    endDate: '2020年12月10日',
-                    endTime: '23:27',
-                    isFinished: false
-                }]
-            }
+export default {
+  data: function () {
+    return {
+      isActive: true,
+      todo: {
+        collapse: false,
+        isShowed: true
+      },
+      todoNum: 16,
+      todoList: [
+        {
+          id: 1,
+          value: 'hhhhha',
+          endDate: '2020年12月10日',
+          endTime: '23:27',
+          isFinished: false
         },
-        methods: {
-            showModal() {
-                console.log('tosoList')
-            }
+        {
+          id: 2,
+          value: '第二个任务hhhhha22',
+          endDate: '',
+          endTime: '23:27',
+          isFinished: false
         }
+      ]
     }
+  },
+  methods: {
+    showModal() {
+      console.log("tosoList")
+    }
+  }
+}
 </script>
