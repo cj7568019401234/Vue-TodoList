1f5b477bee3a86b64d5f1b7a1a67047b85ac2a7f
liucaijun1 <liucaijun@jd.com>
2020-08-28 08:51:56 +0800

    custom checkbox style

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -9,7 +9,7 @@
         <div v-for="item in todoList" :key="item.id" class="list">
           <div v-if="!item.isFinished" class="item-container list" @click="EditTodo">
             <div class="center item-left">
-              <input type="checkbox" class="item__check" :checked="item.isFinished ? 'checked' : ''" @click="ToggleTodo" v-model="item.id"/>
+                <input type="checkbox" :checked="item.isFinished ? 'checked' : ''" @click="ToggleTodo" v-model="item.id"/>
               <div class="item__main">
                 <label class="item__main__text">{{item.value}}</label>
                 <label v-if="item.endDate" class="item__main__date">{{item.endDate}}</label>
@@ -26,28 +26,28 @@
       </div>
     </div>
     <div class="donelist">
-      <!-- <div
-        class="[collapse-panel,collapse-panel--done,done.isShowed ? '' : 'list--done']"
+      <div
+        class='collapse-panel,collapse-panel--done'  :class="{'list--done' :done.isShowed}"
         bindtap="handleCollapseEvent"
         data-type="done"
       >
-        <i class="[spread done.collapse ? 'collapse-rotate' : '']"></i>已完成
-        <i class="done-icon"></i>
+        <div class='spread' :class="{ 'collapse-rotate' : done.collapse}"></div>已完成
+        <div class="done-icon"></div>
         {{doneNum}}
       </div>
-      <block v-if="done.isShowed" wx:for={{todoList}} wx:key="id" data={{item}} class="list">
+      <block v-if="done.isShowed" v-for="item in todoList" :key="item.id" data='item' class="list">
         <div
           v-if="item.isFinished"
           class="item-container list"
           bindtap="bindEdit"
-          data-id={{item.id}}
+          data-id="item.id"
         >
           <div class="center item-left">
-            <checkbox
-              class="item__check"
+            <input 
+              type='checkbox'
               checked="item.isFinished ? 'checked' : ''"
               catchtap="bindToggle"
-              data-id={{item.id}}
+              data-id="item.id"
             />
             <div class="item__main">
               <text class="item__main__text">{{item.value}}</text>
@@ -55,48 +55,51 @@
               <text v-if="item.endTime" class="end-text item__main__time">{{item.endTime}}</text>
             </div>
           </div>
-          <i class="del-icon" catchtap="bindDelete" data-id={{item.id}}></i>
+          <i class="del-icon" catchtap="bindDelete" data-id="item.id"></i>
         </div>
       </block>
       <div v-if="!doneNum && done.isShowed" class="empty center list--done">
-        <i class="box-icon"></i>
+        <div class="box-icon"></div>
         <text>空空如也~</text>
-      </div>-->
+      </div>
     </div>
   </div>
 </template>
 <script>
-export default {
-  data: function () {
-    return {
-      isActive: true,
-      todo: {
-        collapse: false,
-        isShowed: true
-      },
-      todoNum: 16,
-      todoList: [
-        {
-          id: 1,
-          value: 'hhhhha',
-          endDate: '2020年12月10日',
-          endTime: '23:27',
-          isFinished: false
+    export default {
+        data: function() {
+            return {
+                isActive: true,
+                todo: {
+                    collapse: false,
+                    isShowed: true
+                },
+                done: {
+                    collapse: false,
+                    isShowed: true
+                },
+                todoNum: 16,
+                doneNum: 10,
+                todoList: [{
+                    id: 1,
+                    value: 'hhhhha',
+                    endDate: '2020年12月10日',
+                    endTime: '23:27',
+                    isFinished: false
+                }, {
+                    id: 2,
+                    value: '第二个任务hhhhha22',
+                    endDate: '',
+                    endTime: '23:27',
+                    isFinished: false
+                }],
+
+            }
         },
-        {
-          id: 2,
-          value: '第二个任务hhhhha22',
-          endDate: '',
-          endTime: '23:27',
-          isFinished: false
+        methods: {
+            showModal() {
+                console.log("tosoList")
+            }
         }
-      ]
     }
-  },
-  methods: {
-    showModal() {
-      console.log("tosoList")
-    }
-  }
-}
 </script>
